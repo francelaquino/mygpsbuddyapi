d83702c33819a2d8b3179ca22aeb00cdbf775a60
francelaquino <francel_aquino@yahoo.com>
2019-04-16 21:53:55 +0300

    953

--- routes/member.js
+++ routes/member.js
@@ -5,6 +5,65 @@ const email = require('../modules/email')
 const uuidv4 = require('uuid/v4');
 const moment = require('moment');
 
+
+router.post('/forgotpassword', (req, res) => {
+    pool.getConnection((errorPool, conn) => {
+        if (errorPool) {
+            res.status(500).json({
+                message: 'Something went wrong!'
+            })
+        } else {
+
+            const queryString = `select uid, firstname from members where email='${req.body.email}' and isverify='Y'`;
+
+            pool.query(queryString, (err, rows, fields) => {
+             
+                if (err) {
+                    res.status(500).json({
+                         message: 'Something went wrong!',
+                    })
+                    conn.release();
+                } else {
+                    if(rows.length<=0){
+                      
+                      res.status(200).json({
+                        message:'Unknown email address!',
+                        completed:false
+                      })  
+                      
+                      conn.release();
+                    }else{
+                      
+                      let uid=uuidv4();
+                      let expiration=moment().add(4,'hours').format("YYYY-MM-DD HH:mm");
+
+                       const queryString = `update members set resettoken='${uid}',resettokenexpiration='${expiration}' where email='${req.body.email}' and isverify='Y'`;
+
+                        pool.query(queryString, (err, rows, fields) => {
+                          
+                          if (err) {
+                            res.status(500).json({
+                                message: 'Something went wrong!',
+                                completed:false
+                            })
+                            conn.release();
+                          } else {
+                            res.status(200).json({
+                                message:'Password reset instruction has been sent to your email address',
+                                completed:true
+                              });
+                              conn.release();
+                          }
+                        })
+                    }
+                }
+            });
+        }
+    });
+
+});
+
+
 router.post('/registration', (req, res) => {
     pool.getConnection((errorPool, conn) => {
         if (errorPool) {
